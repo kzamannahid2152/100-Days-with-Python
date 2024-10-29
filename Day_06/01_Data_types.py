4162d2ed8e3dd603d79bbc67024a7ef17a05448f
Md Kamruzzaman <112748384+kzamannahid2152@users.noreply.github.com>
2024-10-29 12:12:30 +0600

    Add files via upload

---
+++ Day_06/01_Data_types.py
@@ -0,0 +1,14 @@
+# a = 123456789 
+# print(a)
+# b = "Harry"
+# print(b)
+# c = True
+# print("Data type of a : ",type(a))
+# print("Data type of b : ",type(b))
+# print("Data type of c : ",type(c))
+
+
+#know more data types like : float, complex etc.
+a = complex(12,8)
+b = 2
+print (a + b)
---
+++ Day_06/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+a = complex(12,8)
+b = 2
+print ("Data type of a : ",type(a + b))
